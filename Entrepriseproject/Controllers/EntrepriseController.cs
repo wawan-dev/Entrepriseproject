42e2a2d6f7f6a1777a869dd6f78a537cec02f16b
Girard Erwan <erwangirard49500@gmail.com>
2025-01-08 09:28:44 +0100

    Recupérer les données del'api en objet

--- Entrepriseproject/Controllers/EntrepriseController.cs
+++ Entrepriseproject/Controllers/EntrepriseController.cs
@@ -1,9 +1,11 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Text.Json;
 using Applicationhackathon;
 using Entrepriseproject.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Entrepriseproject.Controllers
 {
@@ -46,11 +48,29 @@ namespace Entrepriseproject.Controllers
             var response = await _httpClient.GetStringAsync(apiUrl);
 
             // Désérialisation de la réponse JSON
-            var apiResponse = JsonSerializer.Deserialize<ApiResponse>(response);
+            EntrepriseResult deserializedData = JsonConvert.DeserializeObject<EntrepriseResult>(response);
 
-            if (apiResponse?.Items != null)
-            {
-                allEntreprises = apiResponse.Items; // Récupérer les entreprises directement
+            foreach(var a in  deserializedData.Results) {
+                var entreprise = new Entreprise
+                {
+                    Nom = a.NomComplet,
+                    Siren = a.Siren,
+                    Siret = a.Siege?.Siret,
+                    Adresse = a.Siege?.Adresse,
+                    Code_postal = a.Siege?.CodePostal,
+                    Ville = a.Siege?.LibelleCommune,
+                    Departement = a.Siege?.Departement,
+                    Region = a.Siege?.Region,
+                    Pays = "France", // Vous pouvez définir la valeur par défaut ou utiliser l'information si disponible
+                    Date_Creation = DateTime.Today,
+                    Forme_Juridique = a.NatureJuridique,
+                    Activite = a.ActivitePrincipale,
+                    code_ape = a.ActivitePrincipale, // ou un autre champ qui pourrait être le code APE
+                    Dirigeants = string.Join(", ", a.Dirigeants.Select(d => $"{d.Prenoms} {d.Nom}"))
+                };
+
+                // Ajouter l'entreprise à la liste
+                allEntreprises.Add(entreprise);
             }
 
             // Retourner les entreprises sous forme de liste dans la vue
@@ -58,9 +78,6 @@ namespace Entrepriseproject.Controllers
         }
 
 
-        public class ApiResponse
-        {
-            public List<Entreprise> Items { get; set; } // Liste des entreprises
-        }
+       
     }
 }
--- Entrepriseproject/Models/EntrepriseViewModel.cs
+++ Entrepriseproject/Models/EntrepriseViewModel.cs
@@ -19,4 +19,140 @@
         public string Activite { get; set; }
         public string code_ape { get; set; }
     }
+
+    public class Result
+    {
+        public string Siren { get; set; }
+        public string NomComplet { get; set; }
+        public string NomRaisonSociale { get; set; }
+        public string? Sigle { get; set; }
+        public int NombreEtablissements { get; set; }
+        public int NombreEtablissementsOuverts { get; set; }
+        public Siege Siege { get; set; }
+        public string ActivitePrincipale { get; set; }
+        public string? CategorieEntreprise { get; set; }
+        public string? CaractereEmployeur { get; set; }
+        public string DateCreation { get; set; }
+        public string? DateFermeture { get; set; }
+        public string DateMiseAJour { get; set; }
+        public string DateMiseAJourInsee { get; set; }
+        public string EtatAdministratif { get; set; }
+        public string NatureJuridique { get; set; }
+        public string SectionActivitePrincipale { get; set; }
+        public List<Dirigeant> Dirigeants { get; set; }
+        public List<MatchingEtablissement> MatchingEtablissements { get; set; }
+        public Finances? Finances { get; set; }
+        public Complements Complements { get; set; }
+    }
+
+    public class Siege
+    {
+        public string ActivitePrincipale { get; set; }
+        public string? ActivitePrincipaleRegistreMetier { get; set; }
+        public string? AnneeTrancheEffectifSalarie { get; set; }
+        public string Adresse { get; set; }
+        public string CaractereEmployeur { get; set; }
+        public string? Cedex { get; set; }
+        public string CodePostal { get; set; }
+        public string Commune { get; set; }
+        public string? ComplementAdresse { get; set; }
+        public string Coordonnees { get; set; }
+        public string DateCreation { get; set; }
+        public string DateDebutActivite { get; set; }
+        public string? DateFermeture { get; set; }
+        public string? DateMiseAJour { get; set; }
+        public string DateMiseAJourInsee { get; set; }
+        public string Departement { get; set; }
+        public string? DistributionSpeciale { get; set; }
+        public string Epci { get; set; }
+        public bool EstSiege { get; set; }
+        public string EtatAdministratif { get; set; }
+        public string GeoAdresse { get; set; }
+        public string GeoId { get; set; }
+        public string? IndiceRepetition { get; set; }
+        public string Latitude { get; set; }
+        public string? LibelleCedex { get; set; }
+        public string LibelleCommune { get; set; }
+        public string? LibelleCommuneEtranger { get; set; }
+        public string? LibellePaysEtranger { get; set; }
+        public string LibelleVoie { get; set; }
+        public string? ListeEnseignes { get; set; }
+        public string NumeroVoie { get; set; }
+        public string Region { get; set; }
+        public string Siret { get; set; }
+        public string StatutDiffusionEtablissement { get; set; }
+        public string? TrancheEffectifSalarie { get; set; }
+        public string TypeVoie { get; set; }
+    }
+
+    public class Dirigeant
+    {
+        public string Nom { get; set; }
+        public string Prenoms { get; set; }
+        public string AnneeDeNaissance { get; set; }
+        public string DateDeNaissance { get; set; }
+        public string Qualite { get; set; }
+        public string Nationalite { get; set; }
+        public string TypeDirigeant { get; set; }
+    }
+
+    public class MatchingEtablissement
+    {
+        public string ActivitePrincipale { get; set; }
+        public bool AncienSiege { get; set; }
+        public string? AnneeTrancheEffectifSalarie { get; set; }
+        public string Adresse { get; set; }
+        public string CaractereEmployeur { get; set; }
+        public string CodePostal { get; set; }
+        public string Commune { get; set; }
+        public string DateCreation { get; set; }
+        public string DateDebutActivite { get; set; }
+        public string? DateFermeture { get; set; }
+        public string Epci { get; set; }
+        public bool EstSiege { get; set; }
+        public string EtatAdministratif { get; set; }
+        public string GeoId { get; set; }
+        public string Latitude { get; set; }
+        public string LibelleCommune { get; set; }
+        public string Longitude { get; set; }
+        public string? NomCommercial { get; set; }
+        public string Region { get; set; }
+        public string Siret { get; set; }
+        public string StatutDiffusionEtablissement { get; set; }
+        public string? TrancheEffectifSalarie { get; set; }
+    }
+
+    public class Finances
+    {
+        // Ajoutez les propriétés spécifiques aux finances si nécessaire
+    }
+
+    public class Complements
+    {
+        public bool ConventionCollectiveRenseignee { get; set; }
+        public bool EstAssociation { get; set; }
+        public bool EstBio { get; set; }
+        public bool EstEntrepreneurIndividuel { get; set; }
+        public bool EstEntrepreneurSpectacle { get; set; }
+        public bool EstEss { get; set; }
+        public bool EstFiness { get; set; }
+        public bool EstOrganismeFormation { get; set; }
+        public bool EstQualiopi { get; set; }
+        public bool EstRge { get; set; }
+        public bool EstServicePublic { get; set; }
+        public bool EstSiae { get; set; }
+        public bool EstSocieteMission { get; set; }
+        public bool EstUai { get; set; }
+    }
+
+    public class EntrepriseResult
+    {
+        public List<Result> Results { get; set; }
+        public int TotalResults { get; set; }
+        public int Page { get; set; }
+        public int PerPage { get; set; }
+        public int TotalPages { get; set; }
+    }
+
+
 }
